9b30883a3589793f2726a84a433f936dcaba5ae7
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-11-10 13:29:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,57 +5,27 @@ from filefifo import Filefifo
 import time
 
 # ==========================
-# Settings
-# ==========================
-
-# OLED
-PIN_I2C_SCL = 15
-PIN_I2C_SDA = 14
-I2C_FREQ = 400000
-OLED_WIDTH = 128
-OLED_HEIGHT = 64
-
-# LEDs
-PIN_LED1 = 22
-PIN_LED2 = 21
-PIN_LED3 = 20
-
-# Rotary Encoder
-PIN_ENCODER_A = 10
-PIN_ENCODER_B = 11
-PIN_BUTTON = 12
-
-# ADC
-PIN_ADC0 = 26
-PIN_ADC1 = 2
-
-# FIFO buffer size
-FIFO_MAX_SIZE = 50
-
-# ==========================
-# Components
+# Settings/components
 # ==========================
 
 # Display
-i2c = I2C(1, scl=Pin(PIN_I2C_SCL), sda=Pin(PIN_I2C_SDA), freq=I2C_FREQ)
-oled = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
 
 # LEDs
-led1 = Pin(PIN_LED1, Pin.OUT)
-led2 = Pin(PIN_LED2, Pin.OUT)
-led3 = Pin(PIN_LED3, Pin.OUT)
+led1 = Pin(22, Pin.OUT)
+led2 = Pin(21, Pin.OUT)
+led3 = Pin(20, Pin.OUT)
 
 # Rotary Encoder and Button
-encoder_A = Pin(PIN_ENCODER_A, Pin.IN, Pin.PULL_UP)
-encoder_B = Pin(PIN_ENCODER_B, Pin.IN, Pin.PULL_UP)
-button = Pin(PIN_BUTTON, Pin.IN, Pin.PULL_UP)
+encoder_A = Pin(10, Pin.IN, Pin.PULL_UP)
+encoder_B = Pin(11, Pin.IN, Pin.PULL_UP)
+button = Pin(12, Pin.IN, Pin.PULL_UP)
 
 # ADC
-adc0 = ADC(Pin(PIN_ADC0))
-adc1 = ADC(Pin(PIN_ADC1))
+adc0 = ADC(Pin(26))
+adc1 = ADC(Pin(2))
 
 # FIFO
-fifo = []
+fifo =[]
 
-
-# Code
