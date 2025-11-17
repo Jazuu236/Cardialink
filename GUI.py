62c863d885240dbed4ef9b42b0438a38f6d52aa7
Morso33 <69962221+Morso33@users.noreply.github.com>
2025-11-17 14:24:27 +0200

    temp

--- GUI.py
+++ GUI.py
@@ -1,1 +1,17 @@
-# This is for GUI
+import time
+
+class cGUI:
+    def __init__(self, oled):
+        self.oled = oled
+
+    def draw_page_init(self):
+        self.oled.fill(0)
+        self.oled.text("Initializing", 0, 0)
+        self.oled.show()
+
+    def draw_main_menu(self, current_selection):
+        self.oled.fill(0)
+        self.oled.text("Encoder:", 0, 0)
+        if fifo:
+            self.oled.text(fifo[-1], 0, 10)
+        self.oled.show()
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from ssd1306 import SSD1306_I2C
 from machine import Pin, ADC, I2C
 from filefifo import Filefifo
 import time
+import GUI
+
+
+#Input handler as globals for now, will fix later -Tom 
+#-1 left, 0 no turn, 1 right, this is also a float that decays towards null.
+INPUT_HANDLER_current_position = 0
+#A decay from 1 or -1 to 0 takes 1000 ms.
+INPUT_HANDLER_last_modified_time = time.ticks_ms()
 
 # ==========================
 # Settings/components
@@ -28,11 +36,44 @@ adc = ADC(Pin(26))
 # FIFO
 fifo =[]
 
+
 # Encoder turn
 def encoder_turn(pin):
+    global INPUT_HANDLER_current_position
+    global INPUT_HANDLER_last_modified_time
+
+    #Decay logic, called on every turn
+    time_diff = time.ticks_ms() - INPUT_HANDLER_last_modified_time
+    decay_amount = (time_diff / 1000.0)
+
+    if (decay_amount > 1):
+        INPUT_HANDLER_current_position = 0
+    elif (INPUT_HANDLER_current_position > 0):
+        INPUT_HANDLER_current_position -= decay_amount
+    elif (INPUT_HANDLER_current_position < 0):
+        INPUT_HANDLER_current_position += decay_amount
+
+    INPUT_HANDLER_last_modified_time = time.ticks_ms()
+
     if encoder_B.value() != encoder_A.value():
-        fifo.append("Right")
+        if (INPUT_HANDLER_current_position < 1):
+            INPUT_HANDLER_current_position += 0.4
     else:
-        fifo.append("Left")
+        if (INPUT_HANDLER_current_position > -1):
+            INPUT_HANDLER_current_position -= 0.4
+        
 
 encoder_A.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=encoder_turn)
+
+def __main__():
+    gui = GUI.cGUI(oled)
+    gui.draw_page_init()
+    while True:
+        gui.draw_main_menu(INPUT_HANDLER_current_position)
+        print(str(INPUT_HANDLER_current_position))
+        time.sleep(0.1)
+
+    
+    
+if __name__ == "__main__":
+    __main__()
