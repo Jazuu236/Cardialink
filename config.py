b5077876425225fda96d99ec72d399b2a258f6c9
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-11-19 10:09:31 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import mip
+import network
+from time import sleep
+
+SSID = "KME759_Group_1"
+PASSWORD = "!Paska123!"
+BROKER_IP = "192.168.1.253"
+
+def connect_wlan():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(SSID, PASSWORD)
+    
+    while wlan.isconnected() == False:
+        print("Connecting...")
+        sleep(1)
+    print("Connection successful. Pico IP:", wlan.ifconfig()[0])
+    
+connect_wlan()
