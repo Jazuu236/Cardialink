3d7699569b2fb97006e42fd39c8d9ef9fff66c52
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-11-20 16:08:52 +0200

    Update HRV.py
    Laskee nyt HRV analyysin peak to peak interval avulla

--- HRV.py
+++ HRV.py
@@ -1,23 +1,31 @@
-# HRV analysis
-def hrv_analysis(hr_list):
-    ppi_list = [60000 / hr for hr in hr_list]
-    
+# HRV analysis from Peak to Peak Interval(PPI)
+def hrv_analysis(ppi_data):
     # Mean PPI
-    mean_ppi = int(round(sum(ppi_list) / len(ppi_list)))
+    mean_ppi = round(sum(ppi_data) / len(ppi_data))
+    mean_ppi = int(mean_ppi)
     
     # Mean HR
     mean_hr = int(round(60000 / mean_ppi))
     
     # SDNN
-    sdnn = round((sum((x - mean_ppi) ** 2 for x in ppi_list) / (len(ppi_list) - 1)) ** 0.5)
+    sdnn = round((sum((x - mean_ppi) ** 2 for x in ppi_data) / (len(ppi_data) - 1)) ** 0.5)
     
     # RMSSD
-    diffs = [(ppi_list[i+1] - ppi_list[i]) ** 2 for i in range(len(ppi_list)-1)]
+    diffs = [(ppi_data[i+1] - ppi_data[i]) ** 2 for i in range(len(ppi_data)-1)]
     rmssd = round((sum(diffs) / len(diffs)) ** 0.5)
 
     return {
-        "mean_ppi": mean_ppi,
-        "mean_hr": mean_hr,
-        "sdnn": sdnn,
-        "rmssd": rmssd
+        "Mean_PPI": mean_ppi,
+        "Mean_HR": mean_hr,
+        "SDNN": sdnn,
+        "RMSSD": rmssd
     }
+# Test
+peak_to_peak_interval_data = [
+    960, 945, 930, 948, 955,
+    970, 980, 965, 950, 940,
+    935, 955, 970, 960, 945,
+    935, 950, 965, 975, 960
+]
+
+print(hrv_analysis(peak_to_peak_interval_data))
